d190d51f4a606881693eefa2d85773608542064a
Blake Thomas <bwthomas@gmail.com>
2013-05-10 08:26:10 -0500

    Redirect root to templates resource.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Stachio::Engine.routes.draw do
   resources :templates
+  root :to => 'templates#index'
 end
--- spec/dummy/config/routes.rb
+++ spec/dummy/config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-
   mount Stachio::Engine => "/"
+  root :to => 'stachio#index'
 end
